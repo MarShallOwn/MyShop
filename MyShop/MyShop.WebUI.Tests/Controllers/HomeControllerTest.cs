c016bcbcce7ab3295c3f5099d30f12c2b5cbd4c7
MarShallOwn <53130198+MarShallOwn@users.noreply.github.com>
2019-08-15 00:32:18 +0200

    Added Tests

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
+using MyShop.WebUI.Tests.Mocks;
+
 
 namespace MyShop.WebUI.Tests.Controllers
 {
@@ -13,16 +18,26 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            //// Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new MockContext<ProductCategory>();
+            HomeController controller = new HomeController(productContext,productCategoryContext);
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
     }
 }
+//// Arrange
+//HomeController controller = new HomeController();
+
+//// Act
+//ViewResult result = controller.Index() as ViewResult;
+
+//// Assert
+//Assert.IsNotNull(result);
